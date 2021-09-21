ab2e916c44e8ecc4a6e7aaf37632073ec954c6a1
kameoman <spiderman.935963@gmail.com>
2021-09-21 21:19:07 +0900

    comp

---
+++ Java2.java
@@ -0,0 +1,9 @@
+public class Java2 {
+  public static void main(String[] args) {
+    int ans = 0;
+    for(int i = 0; i < 100+1; i = i +2){
+      ans = ans +i;
+    }
+    System.out.println(ans);
+  }
+}
