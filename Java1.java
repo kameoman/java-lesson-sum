78d30a3f110efc2fe0b69fadc6265ea3e62d1bf9
kameoman <spiderman.935963@gmail.com>
2021-09-21 21:12:03 +0900

    comp

--- Java1.java
+++ Java1.java
@@ -1,7 +1,9 @@
 public class Java1 {
   public static void main(String[] args) {
+    int ans = 0;
     for(int i = 1; i < 100+1; i++){
-      System.out.println(i);
+      ans = ans +i;
     }
+    System.out.println(ans);
   }
 }
