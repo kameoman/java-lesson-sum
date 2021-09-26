c359a7e578f30130c6a8856de1b9b6d368412aea
kameoman <spiderman.935963@gmail.com>
2021-09-26 15:16:46 +0900

    lesson-5-2

--- Lesson2.java
+++ Lesson2.java
@@ -39,14 +39,27 @@ public class Lesson2 {
     // System.out.println(ans);
     // double aves = (double)ans/5;
     // System.out.println(aves);
-    System.out.println("整数を入力してください");
-    String str = br.readLine();
-    int num = Integer.parseInt(str);
-    System.out.println(num);
-    if (num % 2 == 0){
-      System.out.println(num + "は偶数です");
+    // System.out.println("整数を入力してください");
+    // String str = br.readLine();
+    // int num = Integer.parseInt(str);
+    // System.out.println(num);
+    // if (num % 2 == 0){
+      //   System.out.println(num + "は偶数です");
+      // }else{
+        //   System.out.println(num + "は奇数です");
+        // }
+    System.out.println("2つの整数を入力してください");
+    String str1 = br.readLine();
+    String str2 = br.readLine();
+    int num1 = Integer.parseInt(str1);
+    int num2 = Integer.parseInt(str2);
+    System.out.println(num1);
+    System.out.println(num2);
+    if (num1 < num2 ){
+      System.out.println(num1 +"より"+num2+ "は大きい値です");
     }else{
-      System.out.println(num + "は奇数です");
+      System.out.println(num1 +"より"+num2+ "は小さい値です");
     }
+
   }
 }
