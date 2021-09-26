88a41c64e6d75c2eafa63888939088b4df6e097d
kameoman <spiderman.935963@gmail.com>
2021-09-26 16:13:06 +0900

    lesson-6-4

--- Lesson2.java
+++ Lesson2.java
@@ -98,12 +98,18 @@ public class Lesson2 {
     //   sum += num;
     // }while(num != 0);
     // System.out.println(sum);
-    for (int i = 1;i <=9;i++){
-      for (int j =1; j <=9; j++){
-        System.out.print(i*j);
-        System.out.print("\t");
+    // for (int i = 1;i <=9;i++){
+    //   for (int j =1; j <=9; j++){
+    //     System.out.print(i*j);
+    //     System.out.print("\t");
+    //   }
+    //   System.out.print("\n");
+    // }
+      for (int i=1; i<=5 ;i++){
+        for(int j = 0; j<i;j++){
+          System.out.print("*");
+        }
+        System.out.print("\n");
       }
-      System.out.print("\n");
-    }
   }
 }
